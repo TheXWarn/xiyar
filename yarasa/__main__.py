689f44c45f41f804a66ee14179e72df90384d140
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-16 12:49:39 +0400

    Update __main__.py

--- yarasa/__main__.py
+++ yarasa/__main__.py
@@ -171,5 +171,5 @@ if __name__ == "__main__":
             
             bilgi(f"\[1] {LANG['NO_LOG']}\n\[4] {LANG['CLOSE']}")
             
-            Cevap = Prompt.ask(f"[bold yellow]{LANG['WHAT_YOU_WANT']}[/]", choices=["1", "2", "3", "4"], default="4")
+            Cevap = Prompt.ask(f"[bold yellow]{LANG['WHAT_YOU_WANT']}[/]", choices=["1", "2", "3"], default="3")
         basarili("Görüşərik qaqulya. :)")
